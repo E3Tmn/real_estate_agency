65815700f5c5188de53c03436ae39f5e891da7f9
E3Tmn <nick1tapinyagin@yandex.ru>
2023-11-28 02:11:29 +0300

    show owner of flat

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Flat, Claim, Owner
 
 
+class OwnersInline(admin.TabularInline):
+    model = Flat.owners_flat.through
+    raw_id_fields = ['owner']
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
@@ -10,7 +14,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
-    raw_id_fields = ['liked_by']
+    inlines = [OwnersInline]
+    raw_id_fields = ['liked_by']  
 
 @admin.register(Claim)
 class ClaimAdmin(admin.ModelAdmin):
